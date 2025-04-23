24a868755ef27dcdf9246309362c455b019b7753
AlfonsoFernandezSola <alfonso.fernandez.sola@alumnos.upm.es>
2025-04-23 14:06:44 +0200

    Delete src directory
    delete

--- src/controladores/excepciones/PlazaOcupada.java
+++
@@ -1,9 +0,0 @@
-package controladores.excepciones;
-
-public class PlazaOcupada extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public PlazaOcupada(String msg) {
-		super(msg);
-	}
-}
